9174302764a9d179f204f0a4739fad79e4089fce
TNTSuperMan <abworld@outlook.jp>
2025-06-06 05:04:18 +0900

    NotFoundエラーとかのハンドリング

--- src/api/app.ts
+++ src/api/app.ts
@@ -1,5 +1,6 @@
-import { swaggerUI } from '@hono/swagger-ui';
-import { OpenAPIHono } from '@hono/zod-openapi';
+import { swaggerUI } from "@hono/swagger-ui";
+import { OpenAPIHono } from "@hono/zod-openapi";
+import { HTTPException } from "hono/http-exception";
 
 const app = new OpenAPIHono();
 
@@ -11,6 +12,23 @@ app.doc("/spec", {
   }
 }).get("/docs", swaggerUI({
   url: "/spec"
-}));
+})).onError((e,c)=>{
+  if(e instanceof HTTPException){
+    if(e.status === 404){
+      return c.json({
+        code: "ResourceNotFound",
+        message: `${c.req.url} does not exist`
+      }, 404)
+    }else return e.getResponse();
+  }else{
+    console.error(e);
+    return c.json({
+      code: "InternalServerError",
+      message: "Internal server error occurred"
+    })
+  }
+}).notFound(()=>{
+  throw new HTTPException(404);
+});
 
 export default app
