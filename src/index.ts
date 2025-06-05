d045ac4992363067ced3e6670983bbefcd80b287
TNTSuperMan <abworld@outlook.jp>
2025-06-05 22:50:19 +0900

    appをapp.tsに移動

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,3 @@
-import { swaggerUI } from '@hono/swagger-ui';
-import { OpenAPIHono } from '@hono/zod-openapi';
+import app from "./app";
 
-const app = new OpenAPIHono();
-
-app.doc("/spec", {
-  openapi: "3.0.0",
-  info: {
-    title: "API document",
-    version: "1.0.0"
-  }
-}).get("/docs", swaggerUI({
-  url: "/spec"
-}));
-
-export default app
+export default app;
