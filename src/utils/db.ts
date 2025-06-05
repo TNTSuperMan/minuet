926bc6c37e29fea440d84833c25968d72b509d11
TNTSuperMan <abworld@outlook.jp>
2025-06-06 04:10:40 +0900

    データベースutilを追加

---
+++ src/utils/db.ts
@@ -0,0 +1,30 @@
+import { z } from "@hono/zod-openapi";
+import { file, write } from "bun";
+import { resolve } from "path";
+
+export const dbSchema = z.object({
+});
+
+let dbCache: z.infer<typeof dbSchema> = {
+    news: []
+};
+const dbFile = file(resolve(__dirname, "db.json"));
+try{
+    dbCache = dbSchema.parse(await dbFile.json());
+}catch{ }
+
+let doWrite = false;
+
+setInterval(() => {
+    if(doWrite){
+        doWrite = false;
+        write(dbFile, JSON.stringify(dbCache));
+    }
+}, 1000);
+
+export const database = new Proxy(dbCache, {
+    set(t,p,v,r){
+        doWrite = true;
+        return Reflect.set(t,p,v,r);
+    }
+});
