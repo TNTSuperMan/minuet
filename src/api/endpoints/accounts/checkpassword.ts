92e91b4eb5adc7a5337341485eec2377f523d668
TNTSuperMan <abworld@outlook.jp>
2025-06-07 11:42:53 +0900

    パスワード判定APIを追加

---
+++ src/api/endpoints/accounts/checkpassword.ts
@@ -0,0 +1,46 @@
+import app from "../../app";
+import { z } from "@hono/zod-openapi";
+
+app.openapi({
+  path: "/accounts/checkpassword/", method: "post",
+  description: "パスワードが適切かを返す",
+  request: {
+    body: {
+      content: {
+        "application/json": {
+          schema: z.object({
+            password: z.string().describe("確認するパスワード")
+          })
+        }
+      }
+    }
+  },
+  responses: {
+    200: {
+      description: "おｋ",
+      content: {
+        "application/json": {
+          schema: z.object({
+            msg: z.enum([
+              "valid password",
+              "invalid password"
+            ])
+          })
+        }
+      }
+    }
+  }
+}, async c => {
+  const { password } = await c.req.json() as { password: string };
+  if(password.length < 6 || // パスワードの長さ判定
+    password.toLowerCase() == "password" || // passwordの大文字小文字の判定
+    password ===  "123456789".substring(0, password.length) || // 123...のような文字の判定
+    password === "0123456789".substring(0, password.length) || // 012...のような文字の判定
+    password === "abcdefghijklmnopqrstuvwxyz".substring(0, password.length) // abc...のような文字の判定 
+  ) return c.json({
+    msg: "invalid password" as const
+  });
+  else return c.json({
+    msg: "valid password" as const
+  });
+})
--- src/api/endpoints/accounts/index.ts
+++ src/api/endpoints/accounts/index.ts
@@ -1,1 +1,2 @@
+import "./checkpassword";
 import "./checkusername";
