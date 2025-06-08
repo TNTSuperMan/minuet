dc44e62c2f4dd3d34d11038e6aed1d9222abb836
TNTSuperMan <abworld@outlook.jp>
2025-06-09 04:26:14 +0900

    セットアップコマンドを更新(環境変数)

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 git clone https://github.com/scratchfoundation/scratch-www src/www/scratch-www
 cd src/www/scratch-www
 bun i
-bun run build
+ROOT_URL=http://localhost:4517 API_HOST=http://localhost:4519 bun run build
 cd -
