23259e9b20d4d4c1ddac8911e59499795a576a7d
TNTSuperMan <abworld@outlook.jp>
2025-06-09 17:01:45 +0900

    CORSの許可ヘッダーにContent-typeを追加

--- src/api/app.ts
+++ src/api/app.ts
@@ -33,7 +33,7 @@ app.doc("/spec", {
 });
 app.use(cors({
   origin: "http://localhost:4517",
-  allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests'],
+  allowHeaders: ['X-Custom-Header', 'Upgrade-Insecure-Requests', 'Content-type'],
   allowMethods: ['POST', 'GET', 'OPTIONS'],
   exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
   maxAge: 600,
