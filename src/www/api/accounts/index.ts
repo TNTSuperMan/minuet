fef8e74994a5547a5906866880842ffe23a29988
TNTSuperMan <abworld@outlook.jp>
2025-06-09 18:51:38 +0900

    ユーザー登録APIを実装(ちょっと途中)

--- src/www/api/accounts/index.ts
+++ src/www/api/accounts/index.ts
@@ -1,1 +1,2 @@
 import "./check_email";
+import "./register_new_user";
---
+++ src/www/api/accounts/register_new_user.ts
@@ -0,0 +1,83 @@
+import { HTTPException } from "hono/http-exception";
+import { validateUsername } from "../../../api/endpoints/accounts/checkusername";
+import app from "../../app";
+import { z } from "@hono/zod-openapi";
+import { isValidPassword } from "../../../api/endpoints/accounts/checkpassword";
+import { database } from "../../../utils/db";
+import { password } from "bun";
+
+const formdataSchema = z.object({
+  username: z.string(),
+  email: z.string().email(),
+  password: z.string(),
+  birth_month: z.string().regex(/^\d+$/),
+  birth_year: z.string().regex(/^\d+$/),
+  under_16: z.enum(["true", "false"]),
+  gender: z.string(),
+  country: z.string(),
+})
+
+const dbReturnSchema = z.object({
+  id: z.number()
+})
+
+app.openapi({
+  path: "/accounts/register_new_user/", method: "post",
+  description: "ユーザーを登録します",
+  request: {
+    body: {
+      content: {
+        "multipart/form-data": {
+          schema: formdataSchema
+        }
+      }
+    }
+  },
+  responses: {
+    200: {
+      description: "おｋ",
+      content: {
+        "application/json": {
+          schema: z.object({
+            username: z.string().describe("登録したユーザー名"),
+            user_id: z.string().describe("割り当てられたユーザーID"),
+            success: z.boolean().describe("成功したか"),
+            token: z.string().optional().describe("アクセストークン"),
+            msg: z.string().describe("メッセージ(正常の場合\"user created\")"),
+            logged_in: z.boolean().describe("そのユーザーにログインしたか(wwwで使われてない気がする)"),
+          })
+        }
+      }
+    }
+  }
+}, async c => {
+  const body = formdataSchema.parse(await c.req.parseBody());
+  const usr_validate_res = validateUsername(body.username);
+  if(usr_validate_res !== "valid username")
+    throw new HTTPException(501, { message: "バリデーションが失敗したときの挙動はまだ☆" });
+  if(!isValidPassword(body.password))
+    throw new HTTPException(400, { message: "パスワードダメだお" });
+
+  const { id } = dbReturnSchema.parse(database.query(`INSERT INTO users (
+    name, birth_month, birth_year, scratchteam, email, password, gender, joined, status, bio, country
+  ) VALUES (?,?,?,?,?,?,?,?,?,?,?) RETURNING id`).get(
+    body.username,
+    parseInt(body.birth_month),
+    parseInt(body.birth_year),
+    0,
+    body.email,
+    await password.hash(body.password),
+    body.gender,
+    new Date().toISOString(),
+    "", "", body.country
+  ))
+
+  return c.json({
+    username: body.username,
+    user_id: id.toString(),
+    success: true,
+    token: "just a moment...",
+    msg: "user created",
+    logged_in: false
+  })
+})
