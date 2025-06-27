9f1a6b5823fe1639a362e0ece38e7c6e6e352047
TNTSuperMan <abworld@outlook.jp>
2025-06-27 15:52:08 +0900

    genTokenを作る

--- src/utils/secret.ts
+++ src/utils/secret.ts
@@ -1,3 +1,15 @@
 import { randomBytes } from "crypto";
+import { sign } from "hono/jwt";
+import { JWTPayload } from "hono/utils/jwt/types";
 
 export const secret = randomBytes(256).toString("ascii");
+
+export const genToken = (payload: JWTPayload, exp: number) => {
+  const now = Math.floor(Date.now() / 1000);
+  return sign({
+    ...payload,
+    exp: now + exp,
+    iat: now,
+    nbf: now,
+  }, secret, "EdDSA");
+}
