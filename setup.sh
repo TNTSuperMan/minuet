c227e87c3a846674aeb2c22bf688b48ce656d1b5
TNTSuperMan <abworld@outlook.jp>
2025-06-28 14:00:39 +0900

    setupをgitではなくzipで入れる

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
-git clone https://github.com/scratchfoundation/scratch-www src/www/scratch-www
+wget https://github.com/scratchfoundation/scratch-www/archive/refs/heads/develop.zip
+unzip develop.zip
+mv scratch-www-develop src/www/scratch-www
+rm develop.zip
+
 cd src/www/scratch-www
 bun i
 ROOT_URL=http://localhost:4517 API_HOST=http://localhost:4519 bun run build
