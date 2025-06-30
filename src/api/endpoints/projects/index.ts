dfac10078a10657c4db8fbcf6c054a150d9b50a2
TNTSuperMan <abworld@outlook.jp>
2025-06-30 16:46:06 +0900

    putでタイトルを変更するAPIを追加(あと色々分離)

--- src/api/endpoints/projects/index.ts
+++ src/api/endpoints/projects/index.ts
@@ -1,9 +1,107 @@
 import { HTTPException } from "hono/http-exception";
-import { getProject } from "../../../utils/project";
-import { getImages, getSigninedUser, getUserWithID, imagesSchema } from "../../../utils/user";
+import { DBProjectSchema, getProject } from "../../../utils/project";
+import { DBUserSchema, getImages, getSigninedUser, getUserWithID, imagesSchema } from "../../../utils/user";
 import app from "../../app";
 import { z } from "@hono/zod-openapi";
 import { genToken } from "../../../utils/secret";
+
+export const projectDataSchema = z.object({
+  author: z.object({
+    history: z.object({
+      joined: z.string().datetime().describe("ユーザーの参加日")
+    }),
+    id: z.number().describe("ユーザーID"),
+    profile: z.object({
+      id: z.number().nullable().describe("プロファイルID(?)"),
+      images: imagesSchema
+    }),
+    scratchteam: z.boolean().describe("Scratchチームかどうか"),
+    username: z.string().describe("ユーザー名")
+  }),
+  comments_allowed: z.boolean().describe("コメントが許可されているか"),
+  description: z.string().describe("メモとクレジット"),
+  history: z.object({
+    created: z.string().datetime().describe("作成日時"),
+    modified: z.string().datetime().describe("編集日時"),
+    shared: z.string().datetime().nullable().describe("共有日時"),
+  }),
+  id: z.number().describe("プロジェクトID"),
+  image: z.string().url().describe("サムネイルURL"),
+  images: z.object({
+    "100x80": z.string().url(),
+    "135x102": z.string().url(),
+    "144x108": z.string().url(),
+    "200x200": z.string().url(),
+    "216x163": z.string().url(),
+    "282x218": z.string().url(),
+  }).describe("サイズごとのサムネイル"),
+  instructions: z.string().describe("使い方"),
+  is_published: z.boolean().describe("プロジェクトが公開されているか"),
+  project_token: z.string().describe("プロジェクトのアクセストークン"),
+  public: z.boolean().describe("プロジェクトが公開されているか"),
+  remix: z.object({
+    parent: z.number().nullable().describe("リミックス元のID"),
+    root: z.number().nullable().describe("リミックスの大本のID"),
+  }),
+  stats: z.object({
+    views: z.number().describe("参照数"),
+    loves: z.number().describe("好き数"),
+    favorites: z.number().describe("お気に入り数"),
+    remixes: z.number().describe("リミックス数")
+  }),
+  title: z.string().describe("タイトル"),
+  visibility: z.string().describe("可視かどうか(visibleだけか?)")
+})
+
+export const getProjectData = async (proj: z.infer<typeof DBProjectSchema>, author: z.infer<typeof DBUserSchema>): Promise<z.infer<typeof projectDataSchema>> => ({
+  author: {
+    history: {
+      joined: new Date(author.joined).toISOString(),
+    },
+    id: author.id,
+    profile: {
+      id: null,
+      images: getImages(author.id),
+    },
+    scratchteam: author.scratchteam !== 0,
+    username: author.name
+  },
+  comments_allowed: proj.comments_allowed !== 0,
+  description: proj.description,
+  history: {
+    created: new Date(proj.created).toISOString(),
+    modified: new Date(proj.modified).toISOString(),
+    shared: proj.shared === null ? null : new Date(proj.shared).toISOString(),
+  },
+  id: proj.id,
+  image: "https://example.com/comming_soon",
+  images: {
+    "100x80": "https://example.com/comming_soon",
+    "135x102": "https://example.com/comming_soon",
+    "144x108": "https://example.com/comming_soon",
+    "200x200": "https://example.com/comming_soon",
+    "216x163": "https://example.com/comming_soon",
+    "282x218": "https://example.com/comming_soon",
+  },
+  instructions: proj.instructions,
+  is_published: proj.public != 0,
+  project_token: await genToken({
+    id: proj.id,
+  }, 60 * 5),
+  public: proj.public != 0,
+  remix: {
+    parent: proj.parent,
+    root: proj.parent, // TODO: ここを修正して
+  },
+  stats: { // TODO: そういう機能ができたら実装
+    views: 1,
+    loves: 0,
+    favorites: 0,
+    remixes: 0,
+  },
+  title: proj.title,
+  visibility: "visible"
+})
 
 app.openapi({
   path: "/projects/:id", method: "get",
@@ -18,53 +116,7 @@ app.openapi({
       description: "おｋ",
       content: {
         "application/json": {
-          schema: z.object({
-            author: z.object({
-              history: z.object({
-                joined: z.string().datetime().describe("ユーザーの参加日")
-              }),
-              id: z.number().describe("ユーザーID"),
-              profile: z.object({
-                id: z.number().nullable().describe("プロファイルID(?)"),
-                images: imagesSchema
-              }),
-              scratchteam: z.boolean().describe("Scratchチームかどうか"),
-              username: z.string().describe("ユーザー名")
-            }),
-            comments_allowed: z.boolean().describe("コメントが許可されているか"),
-            description: z.string().describe("メモとクレジット"),
-            history: z.object({
-              created: z.string().datetime().describe("作成日時"),
-              modified: z.string().datetime().describe("編集日時"),
-              shared: z.string().datetime().nullable().describe("共有日時"),
-            }),
-            id: z.number().describe("プロジェクトID"),
-            image: z.string().url().describe("サムネイルURL"),
-            images: z.object({
-              "100x80": z.string().url(),
-              "135x102": z.string().url(),
-              "144x108": z.string().url(),
-              "200x200": z.string().url(),
-              "216x163": z.string().url(),
-              "282x218": z.string().url(),
-            }).describe("サイズごとのサムネイル"),
-            instructions: z.string().describe("使い方"),
-            is_published: z.boolean().describe("プロジェクトが公開されているか"),
-            project_token: z.string().describe("プロジェクトのアクセストークン"),
-            public: z.boolean().describe("プロジェクトが公開されているか"),
-            remix: z.object({
-              parent: z.number().nullable().describe("リミックス元のID"),
-              root: z.number().nullable().describe("リミックスの大本のID"),
-            }),
-            stats: z.object({
-              views: z.number().describe("参照数"),
-              loves: z.number().describe("好き数"),
-              favorites: z.number().describe("お気に入り数"),
-              remixes: z.number().describe("リミックス数")
-            }),
-            title: z.string().describe("タイトル"),
-            visibility: z.string().describe("可視かどうか(visibleだけか?)")
-          })
+          schema: projectDataSchema
         }
       }
     }
@@ -80,53 +132,5 @@ app.openapi({
 
   const author = getUserWithID(proj.author)!;
 
-  return c.json({
-    author: {
-      history: {
-        joined: new Date(author.joined).toISOString(),
-      },
-      id: author.id,
-      profile: {
-        id: null,
-        images: getImages(author.id),
-      },
-      scratchteam: author.scratchteam !== 0,
-      username: author.name
-    },
-    comments_allowed: proj.comments_allowed !== 0,
-    description: proj.description,
-    history: {
-      created: new Date(proj.created).toISOString(),
-      modified: new Date(proj.modified).toISOString(),
-      shared: proj.shared === null ? null : new Date(proj.shared).toISOString(),
-    },
-    id: proj.id,
-    image: "https://example.com/comming_soon",
-    images: {
-      "100x80": "https://example.com/comming_soon",
-      "135x102": "https://example.com/comming_soon",
-      "144x108": "https://example.com/comming_soon",
-      "200x200": "https://example.com/comming_soon",
-      "216x163": "https://example.com/comming_soon",
-      "282x218": "https://example.com/comming_soon",
-    },
-    instructions: proj.instructions,
-    is_published: proj.public != 0,
-    project_token: await genToken({
-      id: proj.id,
-    }, 60 * 5),
-    public: proj.public != 0,
-    remix: {
-      parent: proj.parent,
-      root: proj.parent, // TODO: ここを修正して
-    },
-    stats: { // TODO: そういう機能ができたら実装
-      views: 1,
-      loves: 0,
-      favorites: 0,
-      remixes: 0,
-    },
-    title: proj.title,
-    visibility: "visible"
-  })
+  return c.json(await getProjectData(proj, author))
 })
---
+++ src/api/endpoints/projects/put.ts
@@ -0,0 +1,48 @@
+import { HTTPException } from "hono/http-exception";
+import { getProject } from "../../../utils/project";
+import { getSigninedUser } from "../../../utils/user";
+import app from "../../app";
+import { z } from "@hono/zod-openapi";
+import { database } from "../../../utils/db";
+import { getProjectData, projectDataSchema } from ".";
+
+app.openapi({
+  path: "/projects/:id", method: "put",
+  description: "プロジェクトのタイトルを変更",
+  request: {
+    params: z.object({
+      id: z.string().regex(/^\d+$/).describe("プロジェクトID")
+    }),
+    body: {
+      content: {
+        "application/json": {
+          schema: z.object({
+            title: z.string().describe("タイトル")
+          })
+        }
+      }
+    }
+  },
+  responses: {
+    200: {
+      description: "おｋ",
+      content: {
+        "application/json": {
+          schema: projectDataSchema
+        }
+      }
+    }
+  }
+}, async c => {
+  const proj = getProject(parseInt(c.req.valid("param").id));
+  const user = await getSigninedUser(c);
+  if(!proj || !user || proj.author !== user.id) throw new HTTPException(403);
+
+  const { title } = c.req.valid("json");
+
+  database.query(
+    "UPDATE projects SET title = ? WHERE id = ?"
+  ).get(title, proj.id);
+
+  return c.json(await getProjectData(proj, user));
+})
