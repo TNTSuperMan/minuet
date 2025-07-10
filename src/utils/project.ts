5924e0ad693a2f0b04358151cd47c92b4662c458
TNTSuperMan <abworld@outlook.jp>
2025-07-11 00:56:08 +0900

    projectユーティリティの型をTypeBoxに

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,27 +1,27 @@
-import { z } from "@hono/zod-openapi";
+import { t } from "elysia";
 import { database } from "./db";
 
-export const DBProjectSchema = z.object({
-  id: z.number(),
-  author: z.number(),
+export const DBProjectSchema = t.Object({
+  id: t.Number(),
+  author: t.Number(),
 
-  created: z.number(),
-  modified: z.number(),
-  shared: z.number().nullable(),
+  created: t.Number(),
+  modified: t.Number(),
+  shared: t.Number().nullable(),
 
-  title: z.string(),
-  description: z.string(),
-  instructions: z.string(),
+  title: t.String(),
+  description: t.String(),
+  instructions: t.String(),
 
-  comments_allowed: z.number(),
-  public: z.number(),
-  thumbnail: z.instanceof(Uint8Array).nullable(),
-  parent: z.number().nullable(),
+  comments_allowed: t.Number(),
+  public: t.Number(),
+  thumbnail: t.Union([t.Uint8Array(), t.Null()]),
+  parent: t.Union([t.Number(), t.Null()]),
   
-  json: z.instanceof(Uint8Array).or(z.string())
+  json: t.Union([t.Uint8Array(), t.String()])
 })
 
-export const getProject = (id: number): z.infer<typeof DBProjectSchema> | null => {
+export const getProject = (id: number): typeof DBProjectSchema.static | null => {
   const projects = database.prepare("SELECT * FROM projects WHERE id = ?").all(id);
   if(!projects.length) return null;
   return DBProjectSchema.parse(projects[0]);
