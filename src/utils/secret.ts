53c64c8542350d5ce908f08fe0932615449a7e23
TNTSuperMan <abworld@outlook.jp>
2025-07-11 00:51:23 +0900

    secretのとこgenTokenをcreateExpireにしとく

--- src/utils/secret.ts
+++ src/utils/secret.ts
@@ -1,18 +1,14 @@
-import { sign } from "hono/jwt";
-import { JWTPayload } from "hono/utils/jwt/types";
-
 //@ts-ignore
 export const key = (await crypto.subtle.generateKey("Ed25519", true, ["sign", "verify"])) as {
   publicKey: CryptoKey,
   privateKey: CryptoKey
 };
 
-export const genToken = (payload: JWTPayload, exp: number) => {
+export const createExpire = (exp: number) => {
   const now = Math.floor(Date.now() / 1000);
-  return sign({
-    ...payload,
+  return {
     exp: now + exp,
     iat: now,
     nbf: now,
-  }, key.privateKey, "EdDSA");
+  };
 }
