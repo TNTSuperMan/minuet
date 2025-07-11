441c96b805ff29c1b3dabff913630858c58ea52b
TNTSuperMan <abworld@outlook.jp>
2025-07-11 23:56:12 +0900

    アセット取得もElysiaに

--- src/ast/endpoints/get.ts
+++ src/ast/endpoints/get.ts
@@ -1,42 +1,27 @@
 import app from "../app";
-import { z } from "@hono/zod-openapi";
 import { path_reg } from "./post";
 import { database } from "../../utils/db";
-import { HTTPException } from "hono/http-exception";
+import { getSchemaValidator, NotFoundError, t } from "elysia";
 
-const assets_schema = z.array(z.object({
-  hash: z.instanceof(Uint8Array),
-  type: z.string(),
-  content: z.instanceof(Uint8Array)
+const assets_schema = t.Array(t.Object({
+  hash: t.Uint8Array(),
+  type: t.String(),
+  content: t.Uint8Array()
 }))
+const parser = getSchemaValidator(assets_schema);
 
-app.openapi({
-  path: "/internalapi/asset/{path}/get/", method: "get",
-  description: "アセットを取得します",
-  request: {
-    params: z.object({
-      path: z.string().regex(path_reg).describe("取得するパス")
-    })
-  },
-  responses: {
-    200: {
-      description: "おｋ",
-      content: {
-        "application/octet-stream": {
-          schema: z.string()
-        }
-      }
-    }
-  }
-}, c => {
-  const { path } = c.req.valid("param");
+app.get("/internalapi/asset/:path/get/", ({ params, set }) => {
+  const { path } = params;
   const [,hash] = path_reg.exec(path)!;
   const hashbin = new Uint8Array(Buffer.from(hash, "hex"));
 
-  const assets = assets_schema.parse(database.prepare(`SELECT * FROM assets WHERE hash = ?`).all(hashbin));
-  if(!assets.length) throw new HTTPException(404);
+  const assets = parser.parse(database.prepare(`SELECT * FROM assets WHERE hash = ?`).all(hashbin));
+  if(!assets.length) throw new NotFoundError();
 
-  return c.body(assets[0].content, 200, {
-    "Content-type": assets[0].type
-  });
+  set.headers["content-type"] = assets[0].type;
+  return assets[0].content;
+}, {
+  params: t.Object({
+    path: t.String({ pattern: path_reg.source, description: "取得するパス" })
+  })
 })
