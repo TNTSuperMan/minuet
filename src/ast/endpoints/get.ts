1da4289d95556fd5fe086c2172bc6f0a7eb3824f
TNTSuperMan <abworld@outlook.jp>
2025-07-12 00:02:14 +0900

    もうparseしなくていいかな()

--- src/ast/endpoints/get.ts
+++ src/ast/endpoints/get.ts
@@ -1,21 +1,20 @@
 import app from "../app";
 import { path_reg } from "./post";
 import { database } from "../../utils/db";
-import { getSchemaValidator, NotFoundError, t } from "elysia";
+import { NotFoundError, t } from "elysia";
 
 const assets_schema = t.Array(t.Object({
   hash: t.Uint8Array(),
   type: t.String(),
   content: t.Uint8Array()
 }))
-const parser = getSchemaValidator(assets_schema);
 
 app.get("/internalapi/asset/:path/get/", ({ params, set }) => {
   const { path } = params;
   const [,hash] = path_reg.exec(path)!;
   const hashbin = new Uint8Array(Buffer.from(hash, "hex"));
 
-  const assets = parser.parse(database.prepare(`SELECT * FROM assets WHERE hash = ?`).all(hashbin)) as any as typeof assets_schema.static;
+  const assets = database.prepare(`SELECT * FROM assets WHERE hash = ?`).all(hashbin) as any as typeof assets_schema.static;
   if(!assets.length) throw new NotFoundError();
 
   set.headers["content-type"] = assets[0].type;
