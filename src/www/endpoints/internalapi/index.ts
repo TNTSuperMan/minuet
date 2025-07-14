83ab3ccb8cca827ff7e71758475afa3cd5a15845
TNTSuperMan <abworld@outlook.jp>
2025-07-14 18:15:11 +0900

    サムネイルAPIのelysiation

--- src/www/endpoints/internalapi/index.ts
+++ src/www/endpoints/internalapi/index.ts
@@ -1,1 +1,5 @@
-import "./project";
+import app from "../../app";
+import { useProjectPlugin } from "./project";
+
+app.group("/internalapi", app =>
+  app.use(useProjectPlugin));
--- src/www/endpoints/internalapi/project/thumbnail.ts
+++ src/www/endpoints/internalapi/project/thumbnail.ts
@@ -1,58 +1,45 @@
-import { HTTPException } from "hono/http-exception";
 import { getProject } from "../../../../utils/project";
-import { getSigninedUser } from "../../../../utils/user";
-import app from "../../../app";
-import { z } from "@hono/zod-openapi";
 import { database } from "../../../../utils/db";
+import { ElysiaApp } from "../../../../utils/app";
+import { t } from "elysia";
 
-app.openapi({
-  path: "/internalapi/project/thumbnail/{id}/set/", method: "post",
-  description: "プロジェクトサムネイルを設定します",
-  request: {
-    params: z.object({
-      id: z.string().regex(/^\d+$/).describe("プロジェクトID"),
-    }),
-    body: {
-      content: {
-        "image/png": {
-          schema: z.string().describe("PNGデータ")
-        }
-      }
+export const useThumbnailPlugin = (app: ElysiaApp) => app.post(
+  "/thumbnail/:id/set/", async ({ params: { id }, body, user, set }) => {
+    const proj = getProject(parseInt(id));
+
+    if(!proj || !user || proj.author !== user.id) {
+      set.status = 403;
+      return "403 Forbidden";
     }
-  },
-  responses: {
-    200: {
-      description: "おｋ",
-      content: {
-        "application/json": {
-          schema: z.object({
-            status: z.string().describe("結果"),
-            "content-length": z.number().describe("サムネイルサイズ"),
-            "content-name": z.string().describe("プロジェクトID"),
-            "autosave-internal": z.string().describe("保存間隔?"),
-            "result-code": z.number().describe("返り値的なの")
-          })
-        }
-      }
+
+    const body_bytes = await body.bytes();
+
+    database.query(
+      "UPDATE projects SET thumbnail = ? WHERE id = ?"
+    ).get(body_bytes, proj.id);
+
+    return {
+      status: "success",
+      "content-length": body_bytes.length,
+      "content-name": proj.id.toString(),
+      "autosave-internal": "120",
+      "result-code": 0
+    }
+  }, {
+    detail: { summary: "プロジェクトサムネイルを設定します" },
+    body: t.File({ type: "image/png" }),
+    param: t.Object({
+      id: t.String({ description: "プロジェクトID" })
+    }),
+    response: {
+      200: t.Object({
+        status: t.String({ description: "結果" }),
+        "content-length": t.Number({ description: "サムネイルサイズ" }),
+        "content-name": t.String({ description: "プロジェクトID" }),
+        "autosave-internal": t.Literal("120"),
+        "result-code": t.Number({ description: "返り値的なの" })
+      }),
+      403: t.Literal("403 Forbidden")
     }
   }
-}, async c => {
-  const proj = getProject(parseInt(c.req.valid("param").id));
-  const user = await getSigninedUser(c);
-
-  if(!proj || !user || proj.author !== user.id) throw new HTTPException(403);
-
-  const body = await(await c.req.blob()).bytes();
-
-  database.query(
-    "UPDATE projects SET thumbnail = ? WHERE id = ?"
-  ).get(body, proj.id);
-
-  return c.json({
-    status: "success",
-    "content-length": body.length,
-    "content-name": proj.id.toString(),
-    "autosave-internal": "120",
-    "result-code": 0
-  })
-})
+)
