e5df19372f6ed706ca3eddab3c3e5d2acc3e0545
TNTSuperMan <abworld@outlook.jp>
2025-07-14 18:28:19 +0900

    check_emailをelysiation

--- src/www/endpoints/accounts/index.ts
+++ src/www/endpoints/accounts/index.ts
@@ -1,3 +1,7 @@
-import "./check_email";
+import app from "../../app";
+import { useCheckEmailPlugin } from "./check_email";
 import "./login";
 import "./register_new_user";
+
+app.group("/account", app => app
+  .use(useCheckEmailPlugin));
