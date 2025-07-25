6830aa3f31a173471260c3f8ad62de59a930baf5
TNTSuperMan <tnt-super-man@outlook.com>
2025-07-25 15:38:37 +0900

    ニュースAPIえおelysiation

---
+++ src/api/endpoints/news.ts
@@ -0,0 +1,24 @@
+import app from "../app";
+import { database } from "../../utils/db";
+import { t } from "elysia";
+
+const newsQuery = database.query("SELECT * FROM news LIMIT ? OFFSET ?");
+
+app.get("/news", ({ query: { limit, offset } }) => newsQuery.all(
+  Math.min(parseInt(limit) || 0, 20),
+  parseInt(offset) || 0,
+) as any, {
+  detail: { summary: "ニュース情報を返します" },
+  query: t.Object({
+    limit: t.String({ format: "regex", pattern: "^\\d+$", description: "表示数の最大(最大・デフォルト20)", examples: [20, 3] }),
+    offset: t.String({ format: "regex", pattern: "^\\d+$", description: "表示のオフセット", examples: [0, 3] }),
+  }),
+  response: t.Array(t.Object({
+    id: t.Number({ description: "ニュースのID" }),
+    stamp: t.String({ format: "date-time", description: "ニュースのタイムスタンプ" }),
+    headline: t.String({ description: "タイトル" }),
+    url: t.String({ format: "uri", description: "ニュースURL" }),
+    image: t.String({ format: "uri", description: "説明画像" }),
+    copy: t.String({ description: "説明" })
+  }))
+});
