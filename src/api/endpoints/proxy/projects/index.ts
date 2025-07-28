0190a9802cd5dc484c3637b7312daaf427e8fb0b
TNTSuperMan <tnt-super-man@outlook.com>
2025-07-28 16:26:50 +0900

    proxy/projectsのelysiation

--- src/api/endpoints/proxy/projects/index.ts
+++ src/api/endpoints/proxy/projects/index.ts
@@ -1,1 +1,5 @@
-import "./share";
+import { ElysiaApp } from "../../../../utils/app";
+import { proxyShareProjectPlugin } from "./share";
+
+export const proxyProjectsPlugin = (app: ElysiaApp) =>
+  app.group("/projects", (app) => app.use(proxyShareProjectPlugin));
