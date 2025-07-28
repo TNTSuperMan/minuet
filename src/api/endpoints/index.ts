4a4b57f2ec92a157ff7716b0f432090b301d72ba
TNTSuperMan <tnt-super-man@outlook.com>
2025-07-28 16:43:12 +0900

    APIサーバーでuseを結合

--- src/api/endpoints/index.ts
+++ src/api/endpoints/index.ts
@@ -1,25 +1,30 @@
 import app from "../app";
 import { t } from "elysia";
+import { accountsRoutes } from "./accounts";
+import { newsRoutes } from "./news";
+import { projectsRoutes } from "./projects";
+import { proxyRoutes } from "./proxy";
 
-import "./accounts";
-import "./news";
-import "./projects";
-import "./proxy";
 import "./users";
 
-app.get(
-  "/",
-  {
-    website: "localhost:4517",
-    api: "localhost:4519",
-    help: "help@example.com",
-  },
-  {
-    detail: { summary: "サーバーの基本情報を返します" },
-    response: t.Object({
-      website: t.String({ format: "uri", description: "ウェブサイトURL" }),
-      api: t.String({ format: "uri", description: "このAPIサーバーのURL" }),
-      help: t.String({ format: "uri", description: "ヘルプ用メールアドレス" }),
-    }),
-  }
-);
+app
+  .use(accountsRoutes)
+  .use(newsRoutes)
+  .use(projectsRoutes)
+  .use(proxyRoutes)
+  .get(
+    "/",
+    {
+      website: "localhost:4517",
+      api: "localhost:4519",
+      help: "help@example.com",
+    },
+    {
+      detail: { summary: "サーバーの基本情報を返します" },
+      response: t.Object({
+        website: t.String({ format: "uri", description: "ウェブサイトURL" }),
+        api: t.String({ format: "uri", description: "このAPIサーバーのURL" }),
+        help: t.String({ format: "uri", description: "ヘルプ用メールアドレス" }),
+      }),
+    }
+  );
