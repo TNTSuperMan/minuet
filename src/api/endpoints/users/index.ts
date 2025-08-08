7f25b6657cf438b62a7e092c391922d7a3eb96f2
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-08 16:46:13 +0900

    user直下をelysiation

--- src/api/endpoints/users/index.ts
+++ src/api/endpoints/users/index.ts
@@ -1,50 +1,37 @@
-import app from "../../app";
-import { HTTPException } from "hono/http-exception";
-import { z } from "@hono/zod-openapi";
 import { getImages, getUser, userSchema } from "../../../utils/user";
+import { ElysiaApp } from "../../../utils/app";
+import { NotFoundError, t } from "elysia";
 
-import "./messages";
-import "./projects";
-
-app.openapi(
-  {
-    path: "/users/{usr}/",
-    method: "get",
-    description: "ユーザー情報を返します",
-    request: {
-      params: z.object({
-        usr: z.string().describe("ユーザー名"),
-      }),
-    },
-    responses: {
-      200: {
-        description: "ユーザーは存在します",
-        content: {
-          "application/json": {
-            schema: userSchema,
-          },
-        },
+export const userRoutes = (app: ElysiaApp) =>
+  app.group("/users/:usr", (app) =>
+    app.get(
+      "/",
+      ({ params: { usr } }) => {
+        const user = getUser(usr);
+        if (!user) throw new NotFoundError();
+        else
+          return {
+            id: user.id,
+            username: user.name,
+            scratchteam: user.scratchteam != 0,
+            history: {
+              joined: new Date(user.joined).toISOString(),
+            },
+            profile: {
+              id: user.id,
+              images: getImages(user.id),
+              status: user.status,
+              bio: user.bio,
+              country: user.country,
+            },
+          };
       },
-    },
-  },
-  (c) => {
-    const user = getUser(c.req.valid("param").usr);
-    if (!user) throw new HTTPException(404);
-    else
-      return c.json({
-        id: user.id,
-        username: user.name,
-        scratchteam: user.scratchteam != 0,
-        history: {
-          joined: new Date(user.joined).toISOString(),
-        },
-        profile: {
-          id: user.id,
-          images: getImages(user.id),
-          status: user.status,
-          bio: user.bio,
-          country: user.country,
-        },
-      });
-  }
-);
+      {
+        detail: { summary: "ユーザー情報を返します" },
+        params: t.Object({
+          usr: t.String({ description: "ユーザー名" }),
+        }),
+        response: userSchema,
+      }
+    )
+  );
