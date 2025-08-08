b7d7015f4b56850aba00fc9c412b01be17837f4d
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-08 18:05:15 +0900

    visibilityをelysiation

--- src/api/endpoints/users/projects/index.ts
+++ src/api/endpoints/users/projects/index.ts
@@ -1,1 +1,8 @@
-import "./visibility";
+import { UserElysiaApp } from "..";
+import { ElysiaAppRoute } from "../../../../utils/app";
+import { userProjectsVisibilityRoutes } from "./visibility";
+
+export type UserProjectsElysiaApp = ElysiaAppRoute<"/projects/:id", UserElysiaApp>;
+
+export const userProjectsRoutes = (app: UserElysiaApp) =>
+  app.group("/projects/:id", (app) => app.use(userProjectsVisibilityRoutes));
--- src/api/endpoints/users/projects/visibility.ts
+++ src/api/endpoints/users/projects/visibility.ts
@@ -1,66 +1,55 @@
-import { HTTPException } from "hono/http-exception";
 import { getProject } from "../../../../utils/project";
-import app from "../../../app";
-import { z } from "@hono/zod-openapi";
-import { getSigninedUser, getUser } from "../../../../utils/user";
+import { UserProjectsElysiaApp } from ".";
+import { getUser } from "../../../../utils/user";
+import { t } from "elysia";
 
-app.openapi(
-  {
-    path: "/users/{usr}/projects/{id}/visibility",
-    method: "get",
-    description: "共有が可能か等の情報を取得する",
-    request: {
-      params: z.object({
-        usr: z.string().describe("ユーザー名"),
-        id: z.string().regex(/^\d+$/).describe("プロジェクトID"),
+export const userProjectsVisibilityRoutes = (app: UserProjectsElysiaApp) =>
+  app.get(
+    "/visivility",
+    ({ user, params: { usr, id } }) => {
+      const param_user = getUser(usr);
+      if (!param_user) return new Response(null, { status: 403 });
+
+      const proj = getProject(parseInt(id));
+      if (!proj) return new Response(null, { status: 403 });
+
+      if (param_user.id !== proj.author) return new Response(null, { status: 403 });
+
+      if (!user) return new Response(null, { status: 403 });
+
+      if (user.id !== param_user.id) return new Response(null, { status: 403 });
+
+      // TODO: 報告機能が出来たら進める
+      return {
+        censored: false,
+        censoredByAdmin: false,
+        censoredByCommunity: false,
+        creatorId: proj.author,
+        deleted: false,
+        messages: "",
+        projectId: proj.id,
+        reshareable: true,
+      };
+    },
+    {
+      detail: { summary: "共有状態を取得します" },
+      params: t.Object({
+        usr: t.String({ description: "ユーザー名" }),
+        id: t.String({
+          pattern: "^\\d+$",
+          description: "プロジェクトID",
+          examples: ["119019019", "1"],
+        }),
       }),
-    },
-    responses: {
-      200: {
-        description: "おｋ",
-        content: {
-          "application/json": {
-            schema: z.object({
-              censored: z.boolean().describe("検閲されているか"),
-              censoredByAdmin: z.boolean().describe("管理者による検閲か"),
-              censoredByCommunity: z.boolean().describe("大量報告による検閲か"),
-              creatorId: z.number().describe("プロジェクト作成者"),
-              deleted: z.boolean().describe("削除されたか"),
-              messages: z.string().optional().describe("検閲メッセージ"),
-              projectId: z.number().describe("対象のプロジェクトID"),
-              reshareable: z.boolean().describe("再共有が可能か"),
-            }),
-          },
-        },
-      },
-    },
-  },
-  async (c) => {
-    const { usr: _usr, id: _id } = c.req.valid("param");
-
-    const usr = getUser(_usr);
-    if (!usr) throw new HTTPException(403);
-
-    const proj = getProject(parseInt(_id));
-    if (!proj) throw new HTTPException(403);
-
-    if (usr.id !== proj.author) throw new HTTPException(403);
-
-    const currentUsr = await getSigninedUser(c);
-    if (!currentUsr) throw new HTTPException(403);
-
-    if (usr.id !== currentUsr.id) throw new HTTPException(403);
-
-    // TODO: 報告機能が出来たらやる
-    return c.json({
-      censored: false,
-      censoredByAdmin: false,
-      censoredByCommunity: false,
-      creatorId: proj.author,
-      deleted: false,
-      messages: "",
-      projectId: proj.id,
-      reshareable: true,
-    });
-  }
-);
+      response: t.Object({
+        censored: t.Boolean({ description: "検閲されているか" }),
+        censoredByAdmin: t.Boolean({ description: "管理者による検閲か" }),
+        censoredByCommunity: t.Boolean({ description: "大量報告による検閲か" }),
+        creatorId: t.Number({ description: "プロジェクト作成者" }),
+        deleted: t.Boolean({ description: "削除されたか" }),
+        messages: t.Optional(t.String({ description: "検閲メッセージ" })),
+        projectId: t.Number({ description: "対象のプロジェクトID" }),
+        reshareable: t.Boolean({ description: "再共有が可能か" }),
+      }),
+    }
+  );
