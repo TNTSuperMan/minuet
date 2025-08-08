226bd7cfc0e4ffe8ea19c3040b50d26fd2d86d65
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-08 18:05:57 +0900

    Elysiationしたルートを取り込む(これで祝APIサーバーElysiationか...?)

--- src/api/endpoints/users/index.ts
+++ src/api/endpoints/users/index.ts
@@ -1,39 +1,44 @@
 import { ElysiaApp, ElysiaAppRoute } from "../../../utils/app";
 import { getImages, getUser, userSchema } from "../../../utils/user";
 import { NotFoundError, t } from "elysia";
+import { userMessagesRoutes } from "./messages";
+import { userProjectsRoutes } from "./projects";
 
 export type UserElysiaApp = ElysiaAppRoute<"/users/:usr", ElysiaApp>;
 
 export const userRoutes = (app: ElysiaApp) =>
   app.group("/users/:usr", (app) =>
-    app.get(
-      "/",
-      ({ params: { usr } }) => {
-        const user = getUser(usr);
-        if (!user) throw new NotFoundError();
-        else
-          return {
-            id: user.id,
-            username: user.name,
-            scratchteam: user.scratchteam != 0,
-            history: {
-              joined: new Date(user.joined).toISOString(),
-            },
-            profile: {
+    app
+      .use(userMessagesRoutes)
+      .use(userProjectsRoutes)
+      .get(
+        "/",
+        ({ params: { usr } }) => {
+          const user = getUser(usr);
+          if (!user) throw new NotFoundError();
+          else
+            return {
               id: user.id,
-              images: getImages(user.id),
-              status: user.status,
-              bio: user.bio,
-              country: user.country,
-            },
-          };
-      },
-      {
-        detail: { summary: "ユーザー情報を返します" },
-        params: t.Object({
-          usr: t.String({ description: "ユーザー名" }),
-        }),
-        response: userSchema,
-      }
-    )
+              username: user.name,
+              scratchteam: user.scratchteam != 0,
+              history: {
+                joined: new Date(user.joined).toISOString(),
+              },
+              profile: {
+                id: user.id,
+                images: getImages(user.id),
+                status: user.status,
+                bio: user.bio,
+                country: user.country,
+              },
+            };
+        },
+        {
+          detail: { summary: "ユーザー情報を返します" },
+          params: t.Object({
+            usr: t.String({ description: "ユーザー名" }),
+          }),
+          response: userSchema,
+        }
+      )
   );
