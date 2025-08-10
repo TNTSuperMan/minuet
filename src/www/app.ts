031ecdb974e4515ea00a38e705c1f509eee56e98
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-10 22:47:37 +0900

    WWWルーティングを修正

--- src/www/app.ts
+++ src/www/app.ts
@@ -1,25 +1,20 @@
-import { handleWWW } from "./www";
+import { WWWPageRoutes } from "./www";
 import { createElysiaApp } from "../utils/app";
 
-const app = createElysiaApp("WWW").onError(async ({ code, set, error, route, redirect }) => {
-  switch (code) {
-    case "NOT_FOUND":
-      if(route){
-        const www = await handleWWW(route);
-        if (www) return typeof www === "string" ? redirect(www) : www;
-
-        set.status = 404;
-        set.headers["content-type"] = "text/html";
-      }
-      return '404<br><a href="/">ホーム</a>';
-    case "INTERNAL_SERVER_ERROR":
-      console.error(error);
-      set.status = 500;
-      return {
-        code: "InternalServerError",
-        message: "Internal server error occurred",
-      };
-  }
-});
+const app = createElysiaApp("WWW")
+  .use(WWWPageRoutes)
+  .onError(async ({ code, set, error, route, redirect }) => {
+    switch (code) {
+      case "NOT_FOUND":
+        return '404<br><a href="/">ホーム</a>';
+      case "INTERNAL_SERVER_ERROR":
+        console.error(error);
+        set.status = 500;
+        return {
+          code: "InternalServerError",
+          message: "Internal server error occurred",
+        };
+    }
+  });
 
 export default app;
--- src/www/www.ts
+++ src/www/www.ts
@@ -1,6 +1,7 @@
 import { resolve } from "path";
 import routes from "./scratch-www/src/routes.json" with { type: "json" };
 import { file } from "bun";
+import { ElysiaApp } from "../utils/app";
 
 const fs_base = resolve(__dirname, "scratch-www", "build");
 const routesMap = routes.map(
@@ -11,13 +12,14 @@ const routesMap = routes.map(
     ] as const
 );
 
-export const handleWWW = async (path: string): Promise<Response | string | null> => {
-  const p = resolve(fs_base, path.substring(1));
-  const f = file(p);
-  if (p.startsWith(fs_base) && (await f.exists())) return new Response(f);
-  else {
-    const route = routesMap.find((e) => e[0].test(path));
-    if (!route) return null;
-    else return typeof route[1] === "string" ? route[1] : new Response(route[1]);
-  }
-};
+export const WWWPageRoutes = (app: ElysiaApp) =>
+  app.all("*", async ({ path }) => {
+    const p = resolve(fs_base, path.substring(1));
+    const f = file(p);
+    if (p.startsWith(fs_base) && (await f.exists())) return new Response(f);
+    else {
+      const route = routesMap.find((e) => e[0].test(path));
+      if (!route) return null;
+      else return typeof route[1] === "string" ? route[1] : new Response(route[1]);
+    }
+  });
