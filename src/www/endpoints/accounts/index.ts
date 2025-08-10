f348af1781eb7dbbe2bd1a757a48dbdd564358e0
TNTSuperMan <tnt-super-man@outlook.com>
2025-08-10 16:40:59 +0900

    loginの抽象度を下げてElysiation

--- src/www/endpoints/accounts/index.ts
+++ src/www/endpoints/accounts/index.ts
@@ -1,6 +1,6 @@
 import app from "../../app";
 import { checkEmailRoutes } from "./check_email";
-import "./login";
+import { accountsLoginRoutes } from "./login";
 import "./register_new_user";
 
-app.group("/account", (app) => app.use(checkEmailRoutes));
+app.group("/account", (app) => app.use(checkEmailRoutes).use(accountsLoginRoutes));
