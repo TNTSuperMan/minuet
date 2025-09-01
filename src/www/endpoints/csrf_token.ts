e005a17ad8a3ae6764bc08d496b63567220f263c
TNTSuperMan <tnt-super-man@outlook.com>
2025-09-02 03:28:18 +0900

    CSRFトークンCookieの訂正(2)

--- src/www/endpoints/csrf_token.ts
+++ src/www/endpoints/csrf_token.ts
@@ -8,8 +8,8 @@ const csrfTokenExpire = 365 * 24 * 60 * 60;
 export const csrfTokenRoutes = (app: ElysiaApp) =>
   app.get(
     "/csrf_token/",
-    async ({ cookie, jwt }) => {
-      cookie.scratchcsrftoken.value = await jwt.sign(createExpire(csrfTokenExpire));
+    async ({ cookie: { csrftoken }, jwt }) => {
+      csrftoken.value = await jwt.sign(createExpire(csrfTokenExpire));
       return "";
     },
     {
