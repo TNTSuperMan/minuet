55b156b2ef414eb53feefb1c7235320b0cb8b034
TNTSuperMan <tnt-super-man@outlook.com>
2025-09-02 03:04:51 +0900

    ポート関係の環境変数化・サーバーURL表示の変更

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { stdin } from "bun";
+import { env, stdin } from "bun";
 import { AnyElysia } from "elysia";
 
 import apiApp from "./api";
@@ -8,12 +8,13 @@ import resApp from "./res";
 import wwwApp from "./www";
 
 const listen = (app: AnyElysia, name: string, port: number) => {
-  app.listen(port);
-  stdin.write(`${name} server is running at http://localhost:${port}\n`);
+  app.listen(port, (server) => {
+    stdin.write(`${name} server is running at ${server.url.toString()}\n`);
+  });
 };
 
-listen(apiApp, "API", 4519);
-listen(wwwApp, "WWW", 4517);
-listen(prjApp, "Prj", 4513);
-listen(resApp, "Res", 4514);
-listen(astApp, "AST", 4518);
+listen(apiApp, "API", parseInt(env.API_PORT ?? "") || 4519);
+listen(wwwApp, "WWW", parseInt(env.WWW_PORT ?? "") || 4517);
+listen(prjApp, "Prj", parseInt(env.PRJ_PORT ?? "") || 4513);
+listen(resApp, "Res", parseInt(env.RES_PORT ?? "") || 4514);
+listen(astApp, "AST", parseInt(env.AST_PORT ?? "") || 4518);
