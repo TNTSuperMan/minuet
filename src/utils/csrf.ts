7e7b83e354c32ec10d73adcc9001d1b0840c5f68
TNTSuperMan <tnt-super-man@outlook.com>
2025-09-02 02:09:31 +0900

    CSRFトークンCookieの名前訂正

--- src/utils/csrf.ts
+++ src/utils/csrf.ts
@@ -2,9 +2,9 @@ import { CSRF } from "bun";
 import Elysia from "elysia";
 
 export const verifyCSRF = () =>
-  new Elysia().onBeforeHandle(({ cookie: { scratchcsrftoken }, headers }) => {
+  new Elysia().onBeforeHandle(({ cookie: { csrftoken }, headers }) => {
     if (!CSRF.verify(headers["x-csrftoken"] ?? "")) {
-      scratchcsrftoken.remove();
+      csrftoken.remove();
       return new Response(null, { status: 403, statusText: "Forbidden" });
     }
   });
