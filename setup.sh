c0cdd32ed81bb8d95f10c5a62bd2ca18c3d3a0ce
TNTSuperMan <tnt-super-man@outlook.com>
2025-09-04 16:59:50 +0900

    minuet-wwwをサブモジュールにする

--- setup.sh
+++
@@ -1,9 +0,0 @@
-wget https://github.com/TNTSuperMan/minuet-www/archive/refs/heads/develop.zip
-unzip develop.zip
-mv minuet-www-develop minuet-www
-rm develop.zip
-
-cd minuet-www
-bun i
-bun run build
-cd -
