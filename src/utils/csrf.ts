b4490cb9884f082f5feeb9b4ad52f77f13d53a8c
TNTSuperMan <tnt-super-man@outlook.com>
2025-09-05 04:14:54 +0900

    CSRFがelysia-jwtで生成、Bun.CSRFで検証というアホタレ状態だったのでBun.CSRFに統一

--- src/utils/csrf.ts
+++ src/utils/csrf.ts
@@ -1,9 +1,15 @@
 import { CSRF } from "bun";
 import Elysia from "elysia";
 
+import { csrfTokenExpire } from "../www/endpoints/csrf_token";
+
 export const verifyCSRF = () =>
   new Elysia().onBeforeHandle(({ cookie: { csrftoken }, headers }) => {
-    if (!CSRF.verify(headers["x-csrftoken"] ?? "")) {
+    if (
+      !CSRF.verify(headers["x-csrftoken"] ?? "", {
+        maxAge: csrfTokenExpire,
+      })
+    ) {
       csrftoken.remove();
       return new Response(null, { status: 403, statusText: "Forbidden" });
     }
--- src/www/endpoints/csrf_token.ts
+++ src/www/endpoints/csrf_token.ts
@@ -1,15 +1,17 @@
+import { CSRF } from "bun";
 import { t } from "elysia";
 
 import { ElysiaApp } from "../../utils/app";
-import { createExpire } from "../../utils/secret";
 
-const csrfTokenExpire = 365 * 24 * 60 * 60;
+export const csrfTokenExpire = 365 * 24 * 60 * 60;
 
 export const csrfTokenRoutes = (app: ElysiaApp) =>
   app.get(
     "/csrf_token/",
-    async ({ cookie: { csrftoken }, jwt }) => {
-      csrftoken.value = await jwt.sign(createExpire(csrfTokenExpire));
+    async ({ cookie: { csrftoken } }) => {
+      csrftoken.value = await CSRF.generate(undefined, {
+        expiresIn: csrfTokenExpire,
+      });
       return "";
     },
     {
