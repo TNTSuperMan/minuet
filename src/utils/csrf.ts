4a6432fcaab0e99f3c92abc1c0c4c3285bfe6fc1
TNTSuperMan <tnt-super-man@outlook.com>
2025-09-05 18:48:52 +0900

    CSRFのデフォルト鍵の長さがちょっと過剰だったので64バイトにする

--- src/utils/csrf.ts
+++ src/utils/csrf.ts
@@ -2,7 +2,7 @@ import { CSRF } from "bun";
 import { Context } from "elysia";
 
 export const csrfTokenExpire = 365 * 24 * 60 * 60;
-export const csrfSecret = process.env.CSRF_SECRET ?? crypto.getRandomValues(Buffer.alloc(256)).toBase64();
+export const csrfSecret = process.env.CSRF_SECRET ?? crypto.getRandomValues(Buffer.alloc(64)).toBase64();
 
 export const verifyCSRF = ({ cookie: { csrftoken }, headers }: Context) => {
   if (
