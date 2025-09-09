2d1c1736ace6eab4e04961cf1225efee279cd25d
TNTSuperMan <tnt-super-man@outlook.com>
2025-09-09 15:23:07 +0900

    wwwの404の挙動を改善

--- src/www/app.ts
+++ src/www/app.ts
@@ -1,9 +1,10 @@
 import { createElysiaApp } from "../utils/app";
 
-const app = createElysiaApp("WWW").onError(async ({ code, status, error }) => {
+const app = createElysiaApp("WWW").onError(async ({ code, status, error, set }) => {
   switch (code) {
     case "NOT_FOUND":
-      return '404<br><a href="/">ホーム</a>';
+      set.headers["content-type"] = "text/html";
+      return `<meta charset="utf-8">404<br><a href="/">ホーム</a>`;
     case "INTERNAL_SERVER_ERROR":
       console.error(error);
       return status(500, {
